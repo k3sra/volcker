ff1ea3ba1a7ff1774e3890174aceea7a2ef3dd95
k3sra <habroosh7@gmail.com>
2025-11-29 00:02:46 +0400

    Add MainWindow with custom chrome and UI logic

---
+++ Volcker/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Volcker;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    // Optional: Drag move if clicking on title bar area (handled by WindowChrome usually, but good to have backup)
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseLeftButtonDown(e);
+        if (e.ButtonState == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+}
