491843f7073a3b3b7bd288706259ea7d23c47a5b
k3sra <habroosh7@gmail.com>
2025-11-29 00:02:44 +0400

    Add App.xaml with global styles and startup logic

---
+++ Volcker/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Volcker.Services;
+using Volcker.ViewModels;
+
+namespace Volcker;
+
+public partial class App : Application
+{
+    public new static App Current => (App)Application.Current;
+    public IServiceProvider Services { get; }
+
+    public App()
+    {
+        Services = ConfigureServices();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        // Services
+        services.AddSingleton<FileService>();
+        services.AddSingleton<FirewallService>();
+        services.AddSingleton<StateService>();
+
+        // ViewModels
+        services.AddSingleton<MainViewModel>();
+
+        // Views
+        services.AddSingleton<MainWindow>();
+
+        return services.BuildServiceProvider();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mainWindow = Services.GetRequiredService<MainWindow>();
+        mainWindow.DataContext = Services.GetRequiredService<MainViewModel>();
+        mainWindow.Show();
+    }
+}
